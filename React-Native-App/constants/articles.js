9df2b170e2ce6df3787c5fe8c07ca3151272c4a9
Anant Jain <anant.j2409@gmail.com>
2019-09-14 17:11:40 -0400

    Removing Cards & Linking To Profile

--- React-Native-App/constants/articles.js
+++ React-Native-App/constants/articles.js
@@ -1,8 +1,7 @@
 export default [
   {
-    title: 'Ice cream is made with carrageenan …',
+    title: 'View Notifications',
     image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'View article',
     horizontal: true
   },
   {
